dea5c1ca705dd3e915ec4a973d8342e23f8d1565
jeancarlos0 <jecarlos.me@gmail.com>
2020-07-25 21:31:06 -0300

    First commit

---
+++ src/entry.js
@@ -0,0 +1,16 @@
+import { MediaPlayer } from 'dashjs';
+
+const players = document.querySelectorAll('.player');
+
+let url = "https://dash.akamaized.net/envivio/Envivio-dash2/manifest.mpd";
+//let url2 = "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd";
+//let player = MediaPlayer().create();
+//let player2 = MediaPlayer().create();
+
+players.forEach(player => {
+    let newPlayer = MediaPlayer().create();
+    newPlayer.initialize(player, url, false)
+});
+
+//player.initialize(document.querySelector('#player1'), url, false);
+//player2.initialize(document.querySelector('#player2'), url2, false); 
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: './dist'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'index.html',
+            template: 'index.html'
+        },),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'categories.html',
+            template: 'categories.html'
+        },),
+        new HtmlWebpackPlugin({
+            inject: true,
+            filename: 'videos.html',
+            template: 'videos.html'
+        },),
+
+    ],
+    /*
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: require.resolve('babel-loader'),
+                query: {
+                    presets: [
+                        'es2015'
+                    ]
+                }
+            }
+        ]
+    }*/
+};
